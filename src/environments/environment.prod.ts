3285a96c4862259bf884fd201ef82184b31aac88
ubuntucsharp <ubuntucsharp@gmail.com>
2022-05-05 17:34:02 +0300

    Added angular fire

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebase: {
+    projectId: 'carvaletbooking-46c6e',
+    appId: '1:1067989431413:web:eff92eca40b712632ef43c',
+    storageBucket: 'carvaletbooking-46c6e.appspot.com',
+    locationId: 'europe-west',
+    apiKey: 'AIzaSyDI2H0JpkmcB2fuUZuLpkMjvRKApGJJWWg',
+    authDomain: 'carvaletbooking-46c6e.firebaseapp.com',
+    messagingSenderId: '1067989431413',
+  },
   production: true
 };
