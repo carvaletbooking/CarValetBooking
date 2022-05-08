b4d93232fc9be93089668ba6c486c167ad2dfd5f
ubuntucsharp <ubuntucsharp@gmail.com>
2022-05-08 22:09:30 +0300

    Improve auth guards

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,18 +2,18 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { LoginComponent } from './login/login.component';
-import { AuthGuard, redirectUnauthorizedTo } from '@angular/fire/auth-guard';
+import { canActivate, redirectUnauthorizedTo, redirectLoggedInTo } from '@angular/fire/auth-guard';
 
 const routes: Routes = [
   { 
     path: 'login', 
-    component: LoginComponent 
+    component: LoginComponent, 
+    ...canActivate(() => redirectLoggedInTo(['']))
   },
   { 
     path: '', 
     component: DashboardComponent,
-    canActivate: [AuthGuard],
-    data: { authGuardPipe: () => redirectUnauthorizedTo(['/login']) },
+    ...canActivate(() => redirectUnauthorizedTo(['/login']))
   },
   {
     path: '**',
